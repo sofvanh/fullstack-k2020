33a7f1b1dac7a1bb99700331f450894689431f86
sofivanhanen <sofia.vanhanen0@gmail.com>
2020-01-25 19:10:09 +0200

    Giving feedback (1.6)

--- osa1-unicafe/src/index.js
+++ osa1-unicafe/src/index.js
@@ -1,19 +1,35 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Button = (props) => {
+  return (
+    <button onClick={props.listener}>{props.text}</button>
+  )
+}
+
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const addGood = () => setGood(good + 1)
+  const addNeutral = () => setNeutral(neutral + 1)
+  const addBad = () => setBad(bad + 1)
+
   return (
     <div>
-      code here
+      <h1>Give feedback!</h1>
+      <Button text="Good" listener={addGood} />
+      <Button text="Neutral" listener={addNeutral} />
+      <Button text="Bad" listener={addBad} />
+      <h2>Statistics</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
     </div>
   )
 }
 
-ReactDOM.render(<App />, 
+ReactDOM.render(<App />,
   document.getElementById('root')
 )
