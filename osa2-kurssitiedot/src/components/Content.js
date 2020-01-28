aa08ebb9d3da8512ade852c0cb0ee1fd772a73f0
sofivanhanen <sofia.vanhanen0@gmail.com>
2020-01-28 13:30:27 +0200

    Components folder (2.1)

---
+++ osa2-kurssitiedot/src/components/Content.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Part from './Part'
+
+const Content = ({ parts }) => {
+    return (
+        <ul>
+            {parts.map(part =>
+                <Part key={part.id} part={part} />
+            )}
+        </ul>
+    )
+}
+
+export default Content
--- osa2-kurssitiedot/src/index.js
+++ osa2-kurssitiedot/src/index.js
@@ -1,31 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.parts[0]} />
-      <Part part={props.parts[1]} />
-      <Part part={props.parts[2]} />
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const Total = (props) => {
   return (
@@ -38,27 +13,34 @@ const Total = (props) => {
 const App = () => {
   const course = {
     name: 'Half Stack application development',
+    id: 1,
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Course on Life',
+        exercises: 10,
+        id: 4
       }
     ]
   }
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Course course={course} />
     </div>
   )
 }
