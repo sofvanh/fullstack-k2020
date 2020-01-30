b44356c37ce3eda01e2863fd849644c7cdf41e3a
sofivanhanen <sofia.vanhanen0@gmail.com>
2020-01-30 14:54:03 +0200

    Adding names works (2.6)

--- osa2-puhelinluettelo/src/components/App.js
+++ osa2-puhelinluettelo/src/components/App.js
@@ -1,24 +1,44 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons] = useState([
+  const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
+  ])
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input
+            value={newName}
+            onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map((person) => 
+          <p key={person.name}>{person.name}</p>
+        )}
+      </ul>
     </div>
   )
 
