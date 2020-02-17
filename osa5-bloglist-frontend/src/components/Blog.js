45312107529b3fafefde1a0250ed4eccfa5a567c
sofivanhanen <sofia.vanhanen0@gmail.com>
2020-02-17 14:20:19 +0200

    More details on blog (5.7)

--- osa5-bloglist-frontend/src/components/Blog.js
+++ osa5-bloglist-frontend/src/components/Blog.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false)
+
+  const hideWhenExpanded = { display: expanded ? 'none' : '' }
+  const showWhenExpanded = { display: expanded ? '' : 'none' }
+
+  const toggleExpanded = () => {
+    setExpanded(!expanded)
+  }
+
   return (
-    <p>
-      <b>{blog.title}</b>, {blog.author}
-    </p>
+    <div>
+      <p>
+        <b>{blog.title}</b>, {blog.author}
+        <div style={hideWhenExpanded}>
+          <button onClick={toggleExpanded}>view</button>
+        </div>
+        <div style={showWhenExpanded}>
+          <button onClick={toggleExpanded}>hide</button>
+          <br />
+          {blog.url}
+          <br />
+          Likes: {blog.likes} <button>like</button>
+          <br />
+          {blog.user.name}
+        </div>
+      </p>
+    </div>
   )
 }
 
