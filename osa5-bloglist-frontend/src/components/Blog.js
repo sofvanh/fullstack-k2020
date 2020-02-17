8c2a2533c349958fd9492a8742c2ffe70b96aa33
sofivanhanen <sofia.vanhanen0@gmail.com>
2020-02-17 13:18:37 +0200

    Reformat with tabsize = 2

--- osa5-bloglist-frontend/src/components/Blog.js
+++ osa5-bloglist-frontend/src/components/Blog.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Blog = ({ blog }) => {
-    return(
-        <p>
-            {blog.title}, {blog.author}
-        </p>
-    )
+  return (
+    <p>
+      {blog.title}, {blog.author}
+    </p>
+  )
 }
 
 export default Blog
--- osa5-bloglist-frontend/src/components/LoginForm.js
+++ osa5-bloglist-frontend/src/components/LoginForm.js
@@ -1,37 +1,37 @@
 import React, { useState } from 'react'
 
 const LoginForm = ({ loginAction }) => {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
-    const onSubmit = (event) => {
-        event.preventDefault()
-        loginAction(username, password)
-        setUsername('')
-        setPassword('')
-    }
+  const onSubmit = (event) => {
+    event.preventDefault()
+    loginAction(username, password)
+    setUsername('')
+    setPassword('')
+  }
 
-    return (
-        <form onSubmit={onSubmit}>
-            <div>
-                Username:
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+        Username:
             <input
-                    type="text"
-                    value={username}
-                    name="Username"
-                    onChange={({ target }) => setUsername(target.value)} />
-            </div>
-            <div>
-                Password:
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)} />
+      </div>
+      <div>
+        Password:
                   <input
-                    type="password"
-                    value={password}
-                    name="Password"
-                    onChange={({ target }) => setPassword(target.value)} />
-            </div>
-            <button type="submit">Login</button>
-        </form>
-    )
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)} />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  )
 }
 
 export default LoginForm
