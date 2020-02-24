9ff7ff38585e8fe4398959db08b43982a9b1ba75
sofivanhanen <sofia.vanhanen0@gmail.com>
2020-02-24 11:19:24 +0200

    Adding works (6.4)

--- osa6-redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6-redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -28,6 +28,8 @@ const reducer = (state = initialState, action) => {
         votes: anecdote.votes + 1
       }
       return state.map(a => a.id !== action.data.id ? a : changedAnecdote)
+    case 'NEW_ANECDOTE':
+      return [...state, action.data]
     default:
       return state
   }
@@ -37,6 +39,17 @@ export const voteAnecdote = (id) => {
   return {
     type: 'VOTE',
     data: { id }
+  }
+}
+
+export const createAnecdote = (content) => {
+  return {
+    type: 'NEW_ANECDOTE',
+    data: {
+      content,
+      id: getId(),
+      votes: 0
+    }
   }
 }
 
