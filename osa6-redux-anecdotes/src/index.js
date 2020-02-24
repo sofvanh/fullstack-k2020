d27102d4493b52bb1d16ad189954888fc5883fbc
sofivanhanen <sofia.vanhanen0@gmail.com>
2020-02-24 13:27:23 +0200

    Notification (6.10)

--- osa6-redux-anecdotes/src/index.js
+++ osa6-redux-anecdotes/src/index.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
 import { Provider } from 'react-redux'
 import App from './App'
-import reducer from './reducers/anecdoteReducer'
+import store from './store'
 
-const store = createStore(reducer, composeWithDevTools())
+const Store = store()
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={Store}>
     <App />
   </Provider>,
   document.getElementById('root')
---
+++ osa6-redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,10 @@
+const initialState = "Hello world!"
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default reducer
