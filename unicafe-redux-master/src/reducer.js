57722b1a4ad41c11a36d0bba95349c306e3a5b4b
sofivanhanen <sofia.vanhanen0@gmail.com>
2020-02-24 10:32:56 +0200

    Finish reducer and tests (6.1)

--- unicafe-redux-master/src/reducer.js
+++ unicafe-redux-master/src/reducer.js
@@ -13,14 +13,20 @@ const counterReducer = (state = initialState, action) => {
         good: state.good + 1
       }
     case 'OK':
-      return state
+      return {
+        ...state,
+        ok: state.ok + 1
+      }
     case 'BAD':
-      return state
+      return {
+        ...state,
+        bad: state.bad + 1
+      }
     case 'ZERO':
-      return state
+      return initialState
     default: return state
   }
-  
+
 }
 
 export default counterReducer
--- unicafe-redux-master/src/reducer.test.js
+++ unicafe-redux-master/src/reducer.test.js
@@ -9,25 +9,50 @@ describe('unicafe reducer', () => {
   }
 
   test('should return a proper initial state when called with undefined state', () => {
-    const state = {}
-    const action = {
-      type: 'DO_NOTHING'
-    }
-
-    const newState = counterReducer(undefined, action)
+    const newState = counterReducer(undefined, { type: 'DO_NOTHING' })
     expect(newState).toEqual(initialState)
   })
 
   test('good is incremented', () => {
-    const action = {
-      type: 'GOOD'
-    }
     const state = initialState
-
     deepFreeze(state)
-    const newState = counterReducer(state, action)
+    const newState = counterReducer(state, { type: 'GOOD' })
     expect(newState).toEqual({
       good: 1,
+      ok: 0,
+      bad: 0
+    })
+  })
+
+  test('ok is incremented', () => {
+    const state = initialState
+    deepFreeze(state)
+    const newState = counterReducer(state, { type: 'OK' })
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const state = initialState
+    deepFreeze(state)
+    const newState = counterReducer(state, { type: 'BAD' })
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('zero works', () => {
+    const state = initialState
+    deepFreeze(state)
+    const midState = counterReducer(state, { type: 'GOOD' })
+    const newState = counterReducer(midState, { type: 'ZERO' })
+    expect(newState).toEqual({
+      good: 0,
       ok: 0,
       bad: 0
     })
