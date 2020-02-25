3bb556d54b7651d5bd50736a302f24ffeb243240
sofivanhanen <sofia.vanhanen0@gmail.com>
2020-02-25 13:46:06 +0200

    AnecdoteForm uses connect (6.20)

--- osa6-redux-anecdotes/src/components/AnecdoteForm.js
+++ osa6-redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,17 +1,16 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification } from '../reducers/notificationReducer'
 
 const AnecdoteForm = (props) => {
-  const dispatch = useDispatch()
 
   const addAnecdote = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
-    dispatch(setNotification('Created ' + content, 5))
+    props.createAnecdote(content)
+    props.setNotification('Created ' + content, 5)
   }
 
   return (
@@ -25,4 +24,9 @@ const AnecdoteForm = (props) => {
   )
 }
 
-export default AnecdoteForm
+const mapDispatchToProps = {
+  createAnecdote,
+  setNotification
+}
+
+export default connect(null, mapDispatchToProps)(AnecdoteForm)
